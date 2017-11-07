f1ab975d65909a94b673bacb26ae2d8f990b49a4
RadCon4 <a.conradharrison@gmail.com>
2017-11-07 11:56:46 -0600

    initial database schema/seeds commit

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured, date)
+VALUES  ('Big Mac', false, CURRENT_TIMESTAMP),
+('Jumbo Jack', false, CURRENT_TIMESTAMP), 
+('Whataburger with Cheese', false, CURRENT_TIMESTAMP)
